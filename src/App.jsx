a819bb48e8a85e05bc40d2cc60e5103502a51ede
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-05-26 23:41:20 +0000

    Atualiza informações pessoais no componente App e adiciona animação de hover para ícones sociais

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Allyson J. Dantas",
+  title: "Android Developer",
+  email: "allysonjeronimo@gmail.com",
+  gitHub: "allysonjeronimo",
+  instagram: "allyson.jd",
+  linkedIn: "allyson-dantas-777a2628",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "allyson_jd",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
